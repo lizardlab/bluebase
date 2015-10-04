7cdc77a3e0bbe925d3f6be059efffba88d31c7e8
Logan Lopez <lizardlab@users.noreply.github.com>
2015-10-03 23:53:12 -0400

    fixed bugs that prevented it from authenticating
    Replacement statement was on the wrong string, so it wasn't making the PHP bcrypt string compatible with python. Also added in close connection method to make sure the mysql connection is closed after authentication.

--- auth.py
+++ auth.py
@@ -6,13 +6,13 @@ import mysql.connector
 from datetime import date
 
 ## Database connection configuration
-datahost = '127.0.0.1'
+datahost = 'localhost'
 datauser = 'bluebase'
 dataname = 'bluebase'
 datapass = 'yourpasswordhere'
 
 # connect to database and create cursor
-conn = mysql.connector.connect(user=datahost, password=datapass, host=datahost, database=dataname)
+conn = mysql.connector.connect(user=datauser, password=datapass, host=datahost, database=dataname)
 cursor = conn.cursor()
 today = date.today()
 
@@ -20,7 +20,6 @@ today = date.today()
 file = open(sys.argv[1], 'r')
 username = file.readline().rstrip('\n')
 password = file.readline().rstrip('\n')
-password = password.replace('$2y$', '$2a$')
 
 # make the query such that it only gets accounts that are not expired or disabled
 query = "SELECT `hashed_pass` FROM `users` WHERE `username` = %s AND (`expires` >= %s OR `expires` IS NULL) AND `disabled` = 0"
@@ -30,12 +29,22 @@ cursor.execute(query, params)
 # we just need one record
 row = cursor.fetchone()
 
+# make sure to close the connection
+def close_connection():
+    cursor.close()
+    conn.close()
+
 #if there isn't anything then there the user doesn't exist, is disabled or expired
 if row != None:
-        # hash the password and make sure they match
-    if bcrypt.hashpw(password, row[0]) == row[0]:
+    hash_pass = row[0]
+    hash_pass = hash_pass.replace('$2y$', '$2a$')
+    # hash the password and make sure they match
+    if bcrypt.hashpw(password, hash_pass) == hash_pass:
+        close_connection()
         sys.exit(0)
     else:
+        close_connection()
         sys.exit(1)
 else:
+    close_connection()
     sys.exit(1)
